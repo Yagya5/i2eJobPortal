4d7db63ea17e72ec60fc5e0f3877810a783d541c
Yagya Dutt Sharma <yagya.sharma@i2econsulting.com>
2023-08-04 21:47:42 +0530

    Initial Commit

---
+++ UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace i2eJobPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
