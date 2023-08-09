673004eafa225f400c086f1cb1ca3a06fe23225b
Yagya Dutt Sharma <yagya.sharma@i2econsulting.com>
2023-08-09 18:22:03 +0530

    Added Model Classes and Home Controller & Views

--- DomainModel/Users/User.cs
+++ DomainModel/Users/User.cs
@@ -8,13 +8,33 @@ namespace DomainModel.Users
 {
     public class User
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
 
-        public string? Name { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? State { get; set; }
 
         public string? City { get; set; }
 
-        public string? State { get; set; }
+        public string? Address { get; set; }
+
+        public string? ProfilePicture { get; set; }
+
+        public int RoleId { get; set; }
+
+        public bool Is_Active { get; set; }  
 
     }
 }
