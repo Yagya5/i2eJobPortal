1a0802bdeaf785ecc1adccafa2c11b3ec2362186
Yagya Dutt Sharma <yagya.sharma@i2econsulting.com>
2023-08-09 18:31:22 +0530

    Added Account Controller

---
+++ UI/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(string UserName, string Password)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult SignUP()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+
+    }
+}
