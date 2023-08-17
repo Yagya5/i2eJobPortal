03ccf21165048c6fc1d0e0cf849ac47afda55114
Harikripa <harikripa.shrimali@i2econsulting.com>
2023-08-17 14:16:20 +0530

    Changes in AppliedJobs model

--- DomainModel/AppliedJobs/AppliedJobs.cs
+++ DomainModel/AppliedJobs/AppliedJobs.cs
@@ -12,24 +12,29 @@ namespace DomainModel.AppliedJobs
 {
     public class DM_AppliedJobs
     {
+        public int AppliedJobId { get; set; }
         public int UserId { get; set; }
         public int JobId { get; set; }
 
         public DateTime DateTime { get; set; }
 
         public string ?Status { get; set; }
-
- 
- 
-
- public string ?FirstName{ get; set; }
- public string ?LastName{ get; set; }
- public string ?Gender{ get; set; }
- public string ?JobTitle{ get; set; }
- public string ?DepartmentName{ get; set; }
- public string ?MinExperience{ get; set; }
-public string? Location { get; set; }
+        public string? Round { get; set; }
+        public  int Id { get; set; }
 
 
-}
+
+
+        public string ?FirstName{ get; set; }
+        public string ?JobTitle{ get; set; }
+        public string ?DepartmentName{ get; set; }
+        public string ?MinExperience{ get; set; }
+        public string ?LastName{ get; set; }
+        public string ?Gender{ get; set; }
+        public string? Location { get; set; }
+        public string ? ProfilePicture { get; set; }
+         public string? Resume { get; set; }
+
+    }
+            
 }
