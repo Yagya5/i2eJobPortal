9d0b7c1b92c96785fefb3ad8188ed8b67d59b293
Yagya Dutt Sharma <yagya.sharma@i2econsulting.com>
2023-08-28 19:18:29 +0530

    Fixed Admin Dashboard Routing, Added Audit User Login-Datagrid, SweetAlert2 New Line Issue Resolved

--- UI/Areas/Admin/Controllers/AuditUserLoginsController.cs
+++ UI/Areas/Admin/Controllers/AuditUserLoginsController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services.Users;
 
 namespace UI.Areas.Admin.Controllers
 {
     public class AuditUserLoginsController : Controller
     {
+        private readonly IUserServices _userServices;
+
+        public AuditUserLoginsController(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetRecentLogins()
+        {
+            var result = _userServices.GetRecentLogins();
+            return Ok(result);
         }
     }
 }
