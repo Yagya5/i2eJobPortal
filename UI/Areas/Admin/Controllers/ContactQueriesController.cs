bd803523531d04fe087410b47507611dee3ceb96
Yagya Dutt Sharma <yagya.sharma@i2econsulting.com>
2023-09-07 18:47:06 +0530

    Few Changes in Account Controller

--- UI/Areas/Admin/Controllers/ContactQueriesController.cs
+++ UI/Areas/Admin/Controllers/ContactQueriesController.cs
@@ -8,34 +8,31 @@ namespace UI.Areas.Admin.Controllers
     [Authorize(Roles = "Super Admin, Admin")]
     public class ContactQueriesController : Controller
     {
-
         private readonly IAuditTrailServices _auditTrailServices;
 
-        public ContactQueriesController(IAuditTrailServices auditTrailServices)
+        public ContactQueriesController(IAuditTrailServices auditTrailServices) // Class constructor & Dependency Injection
         {
             _auditTrailServices = auditTrailServices;  
         }
 
-        public IActionResult Index()
+        public IActionResult Index() // It will return Index Page where dxDataGrid will be shown
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult InsertContactQuery(ContactQuery query)
+        public IActionResult InsertContactQuery(ContactQuery query) // It will insert contact-form data into Table_ContactQueries
         {
             var result = _auditTrailServices.InsertContactQuery(query, this.HttpContext);
             return View();
         }
 
-
         [HttpGet]        
-        public IActionResult GetContactQueries()
+        public IActionResult GetContactQueries() // It will fetch Table_ContactQueries records
         {
             var result = _auditTrailServices.GetContactQueries();
             return Ok(result);
         }
-
 
     }
 }
